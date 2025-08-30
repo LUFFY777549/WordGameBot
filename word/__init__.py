78b0661a7b1a8aa9b3dbcfa0af350a8e979a90a8
LUFFY777549 <dragonsimagine1212@gmail.com>
2025-08-30 11:03:03 +0530

    Update __init__.py

--- word/__init__.py
+++ word/__init__.py
@@ -1,6 +1,6 @@
 import os
+import logging
 from pyrogram import Client
-import logging  
 from motor.motor_asyncio import AsyncIOMotorClient
 import pyromod
 from word.modules.word import load_words, load_common_words, load_state_city_countries
@@ -13,15 +13,16 @@ logging.basicConfig(
 )
 
 logging.getLogger("apscheduler").setLevel(logging.ERROR)
-logging.getLogger('httpx').setLevel(logging.WARNING)
+logging.getLogger("httpx").setLevel(logging.WARNING)
 logging.getLogger("pyrate_limiter").setLevel(logging.ERROR)
 LOGGER = logging.getLogger(__name__)
 
 
-TOKEN = os.getenv("TOKEN","8439042357:AAFMXd3_5-fahojjPVGBvRo4EZ7Qgr2Ldps")
-mongo_url = os.getenv("MONGO_URL","mongodb+srv://sufyan532011:5042@auctionbot.5ms20.mongodb.net/?retryWrites=true&w=majority&appName=AuctionBot")
-API_HASH = os.getenv("API_HASH","3474a18b61897c672d315fb330edb213")
-API_ID = os.getenv("API_ID","21218274")
+TOKEN = os.getenv("TOKEN", "8439042357:AAFMXd3_5-fahojjPVGBvRo4EZ7Qgr2Ldps")
+mongo_url = os.getenv("MONGO_URL", "mongodb+srv://sufyan532011:5042@auctionbot.5ms20.mongodb.net/?retryWrites=true&w=majority&appName=AuctionBot")
+API_HASH = os.getenv("API_HASH", "3474a18b61897c672d315fb330edb213")
+API_ID = int(os.getenv("API_ID", "21218274"))   # ✅ integer banaya
+
 if not TOKEN or not mongo_url or not API_HASH or not API_ID:
     raise ValueError("Please set the environment variables: TOKEN, MONGO_URL, API_HASH, and API_ID.")
 
@@ -38,9 +39,9 @@ word = Client(
 DEV_LIST = [7576729648]
 
 client = AsyncIOMotorClient(mongo_url)
-db = client['WordNWord']
-user_Collection = db['user']
-collection = db['word']
+db = client["WordNWord"]
+user_Collection = db["user"]
+collection = db["word"]
 
 WORD_LIST = set(load_words())
 WORD_SET = set(WORD_LIST)
@@ -51,5 +52,6 @@ COUNTRY_SET = set(STATE_CITY_COUNTRY["countries"])
 STATE_SET = set(STATE_CITY_COUNTRY["states"])
 CITY_SET = set(STATE_CITY_COUNTRY["cities"])
 ALL_COUNTRY_SET = COUNTRY_SET | STATE_SET | CITY_SET
+
 print(f"Loaded {len(COUNTRY_SET)} countries, {len(STATE_SET)} states, and {len(CITY_SET)} cities, and total {len(ALL_COUNTRY_SET)}.")
 print(f"Loaded {len(WORD_SET)} words from the word list.")
